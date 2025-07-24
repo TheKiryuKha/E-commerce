7bb60893c48dd94766545779c0afdaf094a70526
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-07-24 18:07:56 +0300

    chore: add UserResource

---
+++ app/Http/Resources/DateResource.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Carbon\CarbonInterface;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property-read CarbonInterface $resource
+ */
+final class DateResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'human' => $this->resource->diffForHumans(),
+            'string' => $this->resource->toDateTimeString(),
+            'local' => $this->resource->toDateTimeLocalString(),
+            'timestamp' => $this->resource->timestamp,
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property-read User $resource
+ */
+final class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'type' => 'user',
+            'attributes' => [
+                'name' => $this->resource->name,
+                'email' => $this->resource->email,
+                'role' => $this->resource->role,
+                'status' => $this->resource->status,
+                'created' => new DateResource(
+                    $this->resource->created_at
+                ),
+            ],
+            'relationships' => [
+                // TODO
+            ],
+            'links' => [
+                // TODO
+            ],
+        ];
+    }
+}
